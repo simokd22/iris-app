1780125795005279d082ca56f4e92de4be015b47
ctekouabou <70098932+ctekouabou@users.noreply.github.com>
2024-11-19 14:09:18 +0100

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -15,6 +15,53 @@ st.set_page_config(
 alt.themes.enable("dark")
 
 # -------------------------
+# Sidebar
+
+# Initialize page_selection in session state if not already set
+if 'page_selection' not in st.session_state:
+    st.session_state.page_selection = 'about'  # Default page
+
+# Function to update page_selection
+def set_page_selection(page):
+    st.session_state.page_selection = page
+
+with st.sidebar:
+
+    st.title('Iris Classification')
+
+    # Page Button Navigation
+    st.subheader("Pages")
+
+    if st.button("About", use_container_width=True, on_click=set_page_selection, args=('about',)):
+        st.session_state.page_selection = 'about'
+    
+    if st.button("Dataset", use_container_width=True, on_click=set_page_selection, args=('dataset',)):
+        st.session_state.page_selection = 'dataset'
+
+    if st.button("EDA", use_container_width=True, on_click=set_page_selection, args=('eda',)):
+        st.session_state.page_selection = "eda"
+
+    if st.button("Data Cleaning / Pre-processing", use_container_width=True, on_click=set_page_selection, args=('data_cleaning',)):
+        st.session_state.page_selection = "data_cleaning"
+
+    if st.button("Machine Learning", use_container_width=True, on_click=set_page_selection, args=('machine_learning',)): 
+        st.session_state.page_selection = "machine_learning"
+
+    if st.button("Prediction", use_container_width=True, on_click=set_page_selection, args=('prediction',)): 
+        st.session_state.page_selection = "prediction"
+
+    if st.button("Conclusion", use_container_width=True, on_click=set_page_selection, args=('conclusion',)):
+        st.session_state.page_selection = "conclusion"
+
+    # Project Details
+    st.subheader("Abstract")
+    st.markdown("A Streamlit dashboard highlighting the results of a training two classification models using the Iris flower dataset from Kaggle.")
+    st.markdown("📊 [Dataset](https://www.kaggle.com/datasets/arshid/iris-flower-dataset)")
+    st.markdown("📗 [Google Colab Notebook](https://colab.research.google.com/drive/1KJDBrx3akSPUW42Kbeepj64ZisHFD-NV?usp=sharing)")
+    st.markdown("🐙 [GitHub Repository](https://github.com/Zeraphim/Streamlit-Iris-Classification-Dashboard)")
+    st.markdown("by: [`Zeraphim`](https://jcdiamante.com)")
+
+# -------------------------
 
 # Load data
 df = pd.read_csv('iris.csv', delimiter=',')

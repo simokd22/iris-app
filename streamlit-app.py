3a7987cbf803c8083b5070442779452f7005aad4
ctekouabou <70098932+ctekouabou@users.noreply.github.com>
2024-11-19 05:09:07 +0100

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -14,21 +14,17 @@ st.header('Iris Data pre-visualization Section')  # Sets a header for a section
 
 # Display data
 st.write(df.head())
-
-my_dataset="iris.csv"
-
 	
 st.subheader('Subsection: Pie Chart Analysis')  # Sets a subheader for a subsection
 
 # Show Dataset
 if st.checkbox("Preview DataFrame"):
-	data = explore_data(my_dataset)
 	if st.button("Head"):
-		st.write(data.head())
+		st.write(df.head())
 	if st.button("Tail"):
-		st.write(data.tail())
+		st.write(df.tail())
 	else:
-		st.write(data.head(2))
+		st.write(df.head(2))
 
 
 # Create chart
@@ -43,8 +39,7 @@ st.write(chart)
 
 # Show Plots
 if st.checkbox("Simple Correlation Plot with Matplotlib "):
-	data = explore_data(my_dataset)
-	plt.matshow(data.corr())
+	plt.matshow(df.corr())
 	st.pyplot()
 
 

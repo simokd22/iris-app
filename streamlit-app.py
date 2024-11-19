e206ae3184f80d159aedb54523f1de96d0f70559
ctekouabou <70098932+ctekouabou@users.noreply.github.com>
2024-11-19 04:40:52 +0100

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -4,7 +4,7 @@ import pandas as pd
 import altair as alt
 
 # Load data
-df = pd.read_csv('https://gist.githubusercontent.com/netj/8836201/raw/6f9306ad21398ea43cba4f7d537619d0e07d5ae3/iris.csv', delimiter=',')
+df = pd.read_csv('iris.csv', delimiter=',')
 
 # Set page title
 st.title('ISJM BI - Iris Dataset Explorer')
@@ -14,7 +14,7 @@ st.write(df.head())
 
 # Show Dataset
 if st.checkbox("Preview DataFrame"):
-	data = explore_data(df)
+	data = explore_data(my_dataset)
 	if st.button("Head"):
 		st.write(data.head())
 	if st.button("Tail"):
@@ -22,11 +22,12 @@ if st.checkbox("Preview DataFrame"):
 	else:
 		st.write(data.head(2))
 
+
 # Create chart
 chart = alt.Chart(df).mark_point().encode(
     x='petal.length',
     y='sepal.width',
-    color='variety'
+    color='species'
 )
 
 # Display chart
